87da642998c916e6a4f721ea1c87c62a540b38d3
Sara22nicole <staed22s@mtholyoke.edu>
2024-07-15 21:01:00 -0400

    adds winner state and game play loop function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,12 @@ console.log("tic tac toe");
 
 
 //these are used to track start of game 
-let turn, board
+let turn, board, winner
 
 //game logic
 
 init()
+//gamePlayLoop()
 //Purpose of init function is to put state of game in a starting position
 function init() {
     turn = "X"
@@ -15,6 +16,7 @@ function init() {
         " ", " ", " ",
         " ", " ", " "
         ]
+    winner = null;
     render()
 
 }
@@ -27,10 +29,21 @@ function render() {
 
 //rendering board
 function renderBoard(){
-    console.log(' 1 |2| 3');
-    console.log(`1 ${board[0]}|${board[1]}|${board[2]}`);
+    console.log(`  ${board[0]}|${board[1]}|${board[2]}`);
     console.log(' --|-|--');
-    console.log(`2 ${board[3]}|${board[4]}|${board[5]}`);
+    console.log(`  ${board[3]}|${board[4]}|${board[5]}`);
     console.log(' --|-|--');
-    console.log(`3 ${board[6]}|${board[7]}|${board[8]}`);
+    console.log(`  ${board[6]}|${board[7]}|${board[8]}`);
+}
+
+//what goes into a turn of tic tac toe. Each turn will follow the same repeated behavior- don't know how long (while loop)
+//1. have to choose an empty space
+//2. put our player's character in that space on the board
+//3. check if we won the game or not
+//4. change turns
+
+function gamePlayLoop(){
+    while (!winner) {
+    //contain all the code for what constitutes one turn
+    }
 }
