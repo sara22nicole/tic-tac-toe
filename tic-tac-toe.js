96026196ad448dc340322dc158d747173d6bee51
Sara22nicole <staed22s@mtholyoke.edu>
2024-07-15 20:41:00 -0400

    adds state variables, emit and render functions, removes index.html filestate files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,36 @@
-console.log('hello, this is tic tac toe');
+console.log("tic tac toe");
+
+
+//these are used to track start of game 
+let turn, board
+
+//game logic
+
+init()
+//Purpose of init function is to put state of game in a starting position
+function init() {
+    turn = "X"
+    board = [
+        " ", " ", " ",
+        " ", " ", " ",
+        " ", " ", " "
+        ]
+    render()
+
+}
+
+function render() {
+//rendering term
+    console.log(`It is player ${turn}'s turn`);
+    
+}
+
+//rendering board
+function renderBoard(){
+    console.log(' 1 |2| 3');
+    console.log(`1 ${board[0]}|${board[1]}|${board[2]}`);
+    console.log(' --|-|--');
+    console.log(`2 ${board[3]}|${board[4]}|${board[5]}`);
+    console.log(' --|-|--');
+    console.log(`3 ${board[6]}|${board[7]}|${board[8]}`);
+}
